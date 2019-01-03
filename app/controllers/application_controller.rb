de0ccd2862ebac219831de3421ea12e03950a2da
Jayesh <jayesh.dateer@gmail.com>
2019-01-03 17:09:45 +0530

    Fixed spaces and indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  
   def category
     @categories = Category.all  
     @category  = Category.find(params[:category_id])
@@ -10,9 +9,8 @@ class ApplicationController < ActionController::Base
 
 
   protected
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob, :mobile])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob, :mobile])  
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob, :mobile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob, :mobile])  
+  end
 end
-
