8a56725162b5bdd8659ef265e90eb94192837d5b
navin malviya <navinmalviya33@gmail.com>
2019-02-20 12:00:48 +0530

    Deleted Test files

--- test/controllers/address_controller_test.rb
+++ test/controllers/address_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CartsControllerTest < ActionDispatch::IntegrationTest
+class AddressControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
--- test/controllers/catogery_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CatogeryControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
